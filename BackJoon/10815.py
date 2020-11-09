62fb7dfc277c1b3050955b2389a8faefafe93718
Choi33 <choi333ys@gmail.com>
2020-11-10 08:17:46 +0900

    백준-10815

--- BackJoon/10815.py
+++ BackJoon/10815.py
@@ -4,21 +4,21 @@ card_list=list(map(int, input().split()))
 check_length=int(input())
 check_list=list(map(int, input().split()))
 
-card_sort=sorted(card_list)
+card_list.sort()
 
 for i in check_list:
     left=0
-    right=card_length
+    right=card_length-1
     flag=False
     while left<=right:
         mid = (left+right) // 2
         if (i==card_list[mid]):
-            print(1)
+            print(1,end=' ')
             flag=True
             break
         elif i<card_list[mid]:
-            left=mid+1
-        else:
             right=mid-1
+        else:
+            left=mid+1
     if(flag==False):
-        print(0)
+        print(0, end=' ')
