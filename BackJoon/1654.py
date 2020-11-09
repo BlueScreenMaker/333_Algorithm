b5bc69eb8123b5701951b3bc62fecddf018a1d0a
Choi33 <choi333ys@gmail.com>
2020-11-10 08:52:14 +0900

    백준-1654

--- BackJoon/1654.py
+++ BackJoon/1654.py
@@ -5,5 +5,16 @@ LAN=[]
 for i in range (0,N):
     LAN.append(int(input()))
 
-LAN.sort()
+left=0
+right=max(LAN)
 
+while left<=right:
+    mid=(left+right)//2
+    total=0
+    for i in LAN:
+        total+=i//mid
+    if total>=K:
+        left=mid+1
+    else:
+        right=mid-1
+print(right)
