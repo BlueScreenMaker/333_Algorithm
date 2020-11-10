b422a0a2b91efd452a64d2fbe9640c08ec9459da
Choi33 <choi333ys@gmail.com>
2020-11-10 09:16:57 +0900

    프로그래머스-폰켓몬 끝

--- Programmers/폰켓몬.py
+++ Programmers/폰켓몬.py
@@ -3,8 +3,15 @@ import itertools
 def solution(nums):
     length=len(nums)
     point=length//2
-
-    answer = 0
+    catch_list=[]
+    for i in nums:
+        if not i in catch_list:
+            if len(catch_list)<=point-1:
+                catch_list.append(i)
+            else:
+                break;
+    # print(catch_list)
+    answer = len(catch_list)
     return answer
 
 print(solution([3,1,2,3]))
