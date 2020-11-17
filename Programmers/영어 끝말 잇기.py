b8a1bd5d084f226da33b00c75c19bd5529289232
Choi33 <choi333ys@gmail.com>
2020-11-17 17:17:59 +0900

    프로그래머스-영어 끝말 잇기:

---
+++ Programmers/영어 끝말 잇기.py
@@ -0,0 +1,8 @@
+def solution(n, words):
+    answer = []
+    flag=len(words)//n
+    print(flag)
+
+    return answer
+
+print(solution(3,["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
