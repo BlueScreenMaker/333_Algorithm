3ed63c62fcb73a99c3075c6d9462bd1a29ccb5a7
Choi33 <choi333ys@gmail.com>
2020-11-17 17:48:27 +0900

    프로그래머스-영어 끝말 잇기

--- Programmers/영어 끝말 잇기.py
+++ Programmers/영어 끝말 잇기.py
@@ -2,6 +2,10 @@ def solution(n, words):
     answer = []
     flag=len(words)//n
     print(flag)
+    slice_list=[]
+    for a in range(0,len(words),flag):
+        slice_list.append(words[a:a+flag])
+    print(slice_list)
 
     return answer
 
