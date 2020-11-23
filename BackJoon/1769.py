5ab7efd04c4480a7210d94458b81bc0f7ef0437e
Choi33 <choi333ys@gmail.com>
2020-11-23 13:31:05 +0900

    수정

--- BackJoon/1769.py
+++ BackJoon/1769.py
@@ -7,13 +7,12 @@ def Counting(num, count):
         Counting(str(total),count+1)
     else:
         total=int(num)
+        # total은 이미 위에서 0값으로 설정되어있음
+        # num을 기준으로 잡아야하니까 total=num
         if total%3==0 and total!=0:
             return print(count,"YES",sep='\n')
         else:
             return print(count,"NO",sep='\n')
-
-
-
 
 num=str(input())
 Counting(num, 0)
