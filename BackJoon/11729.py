27d72adb73ed602d122cbd8034845d5a899c43b0
Choi33 <choi333ys@gmail.com>
2020-11-23 17:23:19 +0900

    백준-11729

---
+++ BackJoon/11729.py
@@ -0,0 +1,10 @@
+def Move_hanoi(n,top1,top2,top3):
+    if count==1:
+        top1=[i for i in range(0,n)]
+    
+    return 0
+
+num=int(input())
+top1,top2,top3=[]
+count=1
+Move_hanoi(num,top1,top2,top3,count)
