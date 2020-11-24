6c2df02459a2e40e69a5423bef2a4833355eb663
Choi33 <choi333ys@gmail.com>
2020-11-24 21:15:23 +0900

    프로그래머스-튜플

--- Programmers/튜플.py
+++ Programmers/튜플.py
@@ -6,14 +6,16 @@ def solution(s):
     for i in range(1,len(string1)):
         if(string1[i]=="}"):
             num=re.findall("\d+",string1[flag:i])
-            num_list.append(num)
+            num_list.append(list(map(int,num)))
             flag=i
+
     num_list.sort(key=lambda x: len(x))
+
     answer = []
     for a in range(0,len(num_list)):
         for b in range(0,len(num_list[a])):
-            if not int(num_list[a][b]) in answer:
-                answer.append(int(num_list[a][b]))
+            if not num_list[a][b] in answer:
+                answer.append(num_list[a][b])
     return answer
 
 print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
