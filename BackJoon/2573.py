42513200da3834b84149632132ba06c78437023b
Choi33 <choi333ys@gmail.com>
2020-12-03 21:33:03 +0900

    백준-2573

--- BackJoon/2573.py
+++ BackJoon/2573.py
@@ -1,8 +1,10 @@
-def Melting(icegerg):
-    return
-
-
 N,M=map(int, input().split())
 iceberg=[]
-for i in range(N):
+for a in range(N):
     iceberg.append(list(map(int, input().split())))
+
+for x in range(0,M):
+    for y in range(0,N):
+        if iceberg[x][y]!=0:
+            if x-1 < -1 or y-1 < -1 or x+1 >= M or y+1 >= N:
+                pass
