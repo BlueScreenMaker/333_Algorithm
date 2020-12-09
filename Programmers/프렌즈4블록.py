8871ca1b8240ba64174a0d5b37541d079dda58bc
Choi33 <choi333ys@gmail.com>
2020-12-09 10:00:20 +0900

    프로그래머스-프랜즈4블록

--- Programmers/프렌즈4블록.py
+++ Programmers/프렌즈4블록.py
@@ -1,8 +1,9 @@
+from collections import deque
+
+way=[[0,-1],[+1,-1],[+2,0],[+2,+1],[0,+2],[+1,+2],[-1,0],[-1,+1]]
+
 def check(m,n,block):
-    if block[m][n]==block[m][n+1]==block[m+1][n+1]:
-        print("옆과 아래에 같은 블록 있음")
-    elif block[m+1][n]==block[m+1][n+1]:
-        print("블록 확인")
+    queue=deque()
 
     return
 
@@ -15,7 +16,7 @@ def solution(m, n, board):
     for i in range(0,m-1):
         for j in range(0,n-1):
             if block[i][j]==block[i][j+1]==block[i+1][j]==block[i+1][j+1]:
-                check(i,j+1,block)
+                check(i,j,block)
                 print("4블록")
     answer = 0
     return answer
