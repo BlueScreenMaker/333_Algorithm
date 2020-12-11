fea25936be6547d33f253b0f79e86353b85ef496
Choi33 <choi333ys@gmail.com>
2020-12-11 13:56:56 +0900

    프로그래머스-프렌즈4블록

--- Programmers/프렌즈4블록.py
+++ Programmers/프렌즈4블록.py
@@ -1,24 +1,42 @@
-def check(m,n,block):
-    if block[m][n]==block[m][n+1]==block[m+1][n+1]:
-        print("옆과 아래에 같은 블록 있음")
-    elif block[m+1][n]==block[m+1][n+1]:
-        print("블록 확인")
-
-    return
-
 def solution(m, n, board):
-    block=[]
-    for a in range(0,m):
-        s=board[a]
-        block.append(list(s))
-
-    for i in range(0,m-1):
-        for j in range(0,n-1):
-            if block[i][j]==block[i][j+1]==block[i+1][j]==block[i+1][j+1]:
-                check(i,j+1,block)
-                print("4블록")
     answer = 0
+    block=[[v for v in z] for z in board]
+
+    flag=True
+    while flag:
+        flag = False
+        same_block = [[False for _ in range(0,n)] for _ in range(0,m)]
+        # print(same_block)
+        for i in range(0,m-1):
+            for j in range(0,n-1):
+                if block[i][j]!='0' and block[i][j]==block[i][j+1]==block[i+1][j]==block[i+1][j+1]:
+                    same_block[i][j]=True
+                    same_block[i][j+1]=True
+                    same_block[i+1][j]=True
+                    same_block[i+1][j+1]=True
+                    flag=True
+
+        for a in range(0,m):
+            for b in range(0,n):
+                if same_block[a][b]==True:
+                    block[a][b]='0'
+                    answer+=1
+        # print(block)
+
+        for y in range(0,n):
+            count=0
+            for x in range(m-1,-1,-1):
+                # print(x,y)
+                if block[x][y]=='0':
+                    count+=1
+                elif count>0:
+                    temp=x+count
+                    block[temp][y]=block[x][y]
+                    block[x][y]='0'
+
+
+
     return answer
 
 
-print(solution(6,6,["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
+print(solution(4,5,["CCBDE", "AAADE", "AAABF", "CCBBF"]))
