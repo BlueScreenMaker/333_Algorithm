b536e69a13cd184da66254856f09488d6b53f366
Choi33 <choi333ys@gmail.com>
2020-12-23 13:59:38 +0900

    백준-17836

--- BackJoon/17836.py
+++ BackJoon/17836.py
@@ -1,4 +1,5 @@
 import sys
+
 from collections import deque
 
 dx=[-1,1,0,0]
@@ -8,35 +9,56 @@ castle=[]
 for a in range(N):
     castle.append(list(map(int, sys.stdin.readline().split())))
 
-test_castle=[]
-for i in range(0,N):
-    for j in range(0,M):
-        castle[i][j]==test_castle[i][j]
-gram=False
+visited=[[False for _ in range(0,M)] for _ in range(0,N)]
+
+
+
 def Path(x,y):
     que=deque()
     que.append((x,y))
-    count=0
+    gram = False
     while que:
         x,y=que.popleft()
         for i in range(0,4):
             nx=x+dx[i]
             ny=y+dy[i]
-            if nx<0 or ny<0 or nx>=N or ny>=M:
-                continue
-            if castle[nx][ny]==1:
-                continue
-            if castle[nx][ny]==0:
-                test_castle[nx][ny]=test_castle[x][y]+1
-                que.append((nx,ny))
-            if castle[nx][ny]==2:
-                gram=True
+            if not gram:
+                if nx<0 or ny<0 or nx>=N or ny>=M:
+                    continue
+                if castle[nx][ny]==1 and visited[nx][ny]==False:
+                    visited[nx][ny]=True
+                    continue
+                if castle[nx][ny]==0 and visited[nx][ny]==False:
+                    castle[nx][ny]=castle[x][y]+1
+                    visited[nx][ny] = True
+                    que.append((nx,ny))
+                if castle[nx][ny]==2 and visited[nx][ny]==False:
+                    gram=True
+                    castle[nx][ny]=0
+            elif gram:
+                if nx<0 or ny<0 or nx>=N or ny>=M:
+                    continue
+                if visited[nx][ny]==False:
+                    if castle[x][y]==1:
+                        castle[nx][ny]+=castle[x][y]
+                    else:
+                        castle[nx][ny]=castle[x][y]+1
+                    visited[nx][ny] = True
+                    que.append((nx,ny))
+
     #         방문시......
     # 마법의 칼 만날 때도 생각해야해
-    return
+    return castle[N-1][M-1]
 
 
+value=Path(0,0)
+if(value<=T):
+    if(value==0):
+        print("Fail")
+    else:
+        print(value)
+else:
+    print("Fail")
 
 
-
-visited=[[False for _ in range(0,M)] for _ in range(0,N)]
+# print(castle)
