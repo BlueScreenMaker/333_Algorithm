5138dd972a84134bf917f90b05fe690c84d632f5
Choi33 <choi333ys@gmail.com>
2021-01-23 15:52:59 +0900

    백업

--- BackJoon/2670.py
+++ BackJoon/2670.py
@@ -1,13 +1,22 @@
 import sys
 
+
 N=int(sys.stdin.readline())
 
 num_list=[]
 for a in range(0,N):
-    num_list.append(list(map(int, sys.stdin.readline().split())))
+    A=float(sys.stdin.readline())
+    num_list.append(A)
 
-save_values=[0]*(N*N)
+save_values=[]
 
-for i in range(0,N):
-    for j in range(i,N):
-        print()
+for i in range(0,N-1):
+    save_values.append(num_list[i])
+    for j in range(i+1,N):
+        a=save_values[-1]*num_list[j]
+        save_values.append(round(a,4))
+    print(save_values)
+    temp1=max(save_values)
+    save_values.clear()
+    save_values.append(temp1)
+print("".join(map(str, save_values)))
