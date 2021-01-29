6a3523d2f620a94de5d1a5a6ab2bc497e0a889a9
Choi33 <choi333ys@gmail.com>
2021-01-29 19:42:15 +0900

    백업

--- BackJoon/2670.py
+++ BackJoon/2670.py
@@ -1,22 +1,31 @@
 import sys
-
+import decimal
 
 N=int(sys.stdin.readline())
-
+ans=-1
 num_list=[]
 for a in range(0,N):
     A=float(sys.stdin.readline())
     num_list.append(A)
 
-save_values=[]
+for a in range(0,N):
+    mul=1
+    for b in range(a,N):
+        mul *= decimal.Decimal(num_list[b])
+        if ans < mul:
+            ans=mul
 
-for i in range(0,N-1):
-    save_values.append(num_list[i])
-    for j in range(i+1,N):
-        a=save_values[-1]*num_list[j]
-        save_values.append(round(a,4))
-    print(save_values)
-    temp1=max(save_values)
-    save_values.clear()
-    save_values.append(temp1)
-print("".join(map(str, save_values)))
+print(round(ans,3))
+# save_values=[0]*N
+# max_valuse=[0]
+#
+#
+# for i in range(0,N-1):
+#     save_values.append(decimal.Decimal(num_list[i])*decimal.Decimal(num_list[i+1]))
+#     for j in range(i+2,N):
+#         save_values.append(decimal.Decimal(save_values[-1]*decimal.Decimal(num_list[j])))
+#     # print(save_values)
+#     temp1=max(save_values)
+#     save_values.clear()
+#     save_values.append(round(temp1,3))
+# print("".join(map(str, save_values)))
