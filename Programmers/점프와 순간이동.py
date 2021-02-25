3f17fcdca230ede28bb213ea1e130cad67dbecbf
Choi33 <choi333ys@gmail.com>
2021-02-25 13:46:05 +0900

    프로그래머스 점프와 순간이동

---
+++ Programmers/점프와 순간이동.py
@@ -0,0 +1,13 @@
+def solution(N):
+    ans = 0
+
+    while N!=0:
+        if N%2==0:
+            N=N/2
+        else:
+            N=N-1
+            ans+=1
+
+    return ans
+
+print(solution(5))
