cb3668b9f13cf64574d1eb5dee186ffbb3a0ec99
Choi33 <choi33ys@gmail.com>
2021-03-19 11:53:14 +0900

    11047_동전 0

---
+++ BackJoon/11047.py
@@ -0,0 +1,19 @@
+import sys
+
+N,K=map(int,sys.stdin.readline().split())
+
+type_coin=[]
+for i in range(N):
+    type_coin.append(int(sys.stdin.readline()))
+
+money=K
+count=0
+for j in range(N-1,-1,-1):
+    count+=money//type_coin[j]
+    money%=type_coin[j]
+    if(money==0):
+        break
+
+print(count)
+
+
