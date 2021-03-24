c17279eb249ae6438bf12621a7ba7ef3471934e9
Choi33 <choi33ys@gmail.com>
2021-03-24 18:53:43 +0900

    큰 수 만들기

--- BackJoon/1931.py
+++ BackJoon/1931.py
@@ -5,39 +5,18 @@ N=int(sys.stdin.readline())
 time_list=[]
 
 count=1
-ans=0
 
 for i in range(0,N):
     time_list.append(list(map(int, sys.stdin.readline().split())))
 
 time_list=sorted(time_list,key=lambda x:(x[1],x[0]))
-print(time_list)
+# 정렬을 x[1] 기준으로 한 번 하고 x[0]으로 다시 하니 되네 왤까??
+
 check=time_list[0][1]
 for i in range(1,N):
     if(check<=time_list[i][0]):
         count+=1
-        print(check,count)
         check=time_list[i][1]
 print(count)
 
-# time_list=sorted(time_list, key=lambda x:(x[0],x[1]))
-# print(time_list)
-
-# check = time_list[0][1]
-# for j in range(1, N):
-#     print("time값", time_list[j][0])
-#     if (check <= time_list[j][0]):
-#         print(j,check)
-#         ans += 1
-#         check = time_list[j][1]
-# for i in range(0,N):
-#     check=time_list[i][1]
-#     for j in range(i,N):
-#         if(check<=time_list[j][0]):
-#             count+=1
-#             check=time_list[j][1]
-#     if ans<count:
-#         ans=count
-#     count = 0
-# print(ans+1)
 
---
+++ Programmers/큰 수 만들기.py
@@ -0,0 +1,33 @@
+import itertools
+
+def solution(number, k):
+    num_list=[]
+    for i in number:
+        while num_list:
+            if i>num_list[-1]:
+                if(k>0):
+                    num_list.pop()
+                    k-=1
+                else:
+                    break
+            else:
+                break
+        num_list.append(i)
+    if k>0:
+        for i in range(k):
+            num_list.pop()
+
+    ans="".join(num_list)
+    return ans
+print(solution("4177252841",4))
+
+
+# def solution(number, k):
+#     num_list=list(number)
+#     length=len(number)
+#     combination=[]
+#     temp=list(map(''.join, itertools.combinations(num_list,length-k)))
+#     combination.extend(temp)
+#     answer = max(combination)
+#     return answer
+
