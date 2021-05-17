7a20b7bc71aeb5e3208e5c2e58a00c95d96740a4
Choi33 <choi33ys@gmail.com>
2021-05-17 12:00:19 +0900

    백준 9536

--- BackJoon/9536.py
+++ BackJoon/9536.py
@@ -4,18 +4,22 @@ import re
 count=int(sys.stdin.readline())
 
 for i in range(count):
-    sound=sys.stdin.readline()
+    sound=sys.stdin.readline().split()
 
     pattern=r'(?P<animal>\w+) (goes) (?P<meowing>\w+)'
     animal_meow=[]
     while True:
         test_case=sys.stdin.readline()
-        print(test_case)
         if test_case.startswith('what'):
             break
         else:
             result=re.match(pattern,test_case)
             animal_meow.append(result.expand('\g<meowing>'))
 
+    ans=[]
+    for a in sound:
+        if a not in animal_meow:
+            ans.append(a)
 
+    print(' '.join(map(str,ans)))
     
