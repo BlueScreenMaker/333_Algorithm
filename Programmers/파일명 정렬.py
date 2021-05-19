eb39258057f5501b5596519d197ebe74436a6e7d
Choi33 <choi33ys@gmail.com>
2021-05-19 15:28:49 +0900

    정규식 완료

---
+++ Programmers/파일명 정렬.py
@@ -0,0 +1,19 @@
+import re
+
+def solution(files):
+
+    #그룹으로 묶으면 숫자가 살아있어  아니 미치고 환장하겠네 진짜
+    #  r'[\d]+'<<이런식으로하면 숫자가 짤려나감;;;
+    pattern = r'([\d]+)'
+    result = []
+    for i in files:
+        check=re.split(pattern,i)
+        result.append(check)
+    result=sorted(result,key=lambda x:(x[0].lower(),int(x[1])))
+
+    answer=[]
+    for i in result:
+        answer.append(''.join(map(str,i)))
+    return answer
+
+print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
