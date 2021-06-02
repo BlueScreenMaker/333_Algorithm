85f7b8d13ef1619a67a21a1772aaf717689efb48
Choi33 <choi33ys@gmail.com>
2021-06-02 20:29:24 +0900

    프로그래머스

--- Programmers/2개 이하로 다른 비트.py
+++ Programmers/2개 이하로 다른 비트.py
@@ -4,8 +4,15 @@ def solution(numbers):
         if i%2==0:
             answer.append(i+1)
         else:
-            bin_num=format(i,'b')
-            print(bin_num)
+            bin_num=str(format(i,'b'))
+            point=bin_num.rfind('01')
+            if point==-1:
+                bin_num='10'+bin_num[1:]
+                print(bin_num)
+                answer.append(int(bin_num,2))
+            else:
+                bin_num=bin_num[:point]+'10'+bin_num[point+2:]
+                answer.append(int(bin_num,2))
     return answer
 
-print(solution({2,7}))
+print(solution({2,45}))
