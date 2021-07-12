066856f4f265062384d4414aa84220757e4bb0ea
Choi33 <choi33ys@gmail.com>
2021-07-12 19:16:56 +0900

    7562 완성

--- BackJoon/7562.py
+++ BackJoon/7562.py
@@ -7,14 +7,18 @@ N=int(sys.stdin.readline())
 def bfs(x, y):
     dx = [[-2, +2], [-1, +1]]
     dy = [[-1, +1], [-2, +2]]
+    '''
+    dx=[-2,-2,+2,+2,-1,+1,-1,+1]
+    dy=[-1,+1,-1,+1,-2,-2,+2,+2]
+    '''
 
     que = deque()
-
     que.append((x, y))
-
 
     while que:
         cx, cy = que.popleft()
+        if cx==wantX and cy==wantY: #이미 cx,cy선에서 원하는 위치에 도달했을 수 있음
+            break
         for i in range(2):
             for j in range(2):
                 nx = cx + dx[i][j]
@@ -22,12 +26,10 @@ def bfs(x, y):
                     ny = cy + dy[i][z]
                     if nx <= -1 or ny <= -1 or nx >= size or ny >= size:
                         continue
-                    if nx == wantX and ny == wantY:
-                        chess[nx][ny] = chess[cx][cy] + 1
-                        break
                     if chess[nx][ny] == 0:
                         chess[nx][ny] = chess[cx][cy]+1
                         que.append((nx, ny))
+
 
 
 for i in range(N):
@@ -35,14 +37,13 @@ for i in range(N):
     currentX,currentY=map(int,sys.stdin.readline().split())
     wantX,wantY=map(int,sys.stdin.readline().split())
 
-    if currentX==wantX and currentY==wantY:
-        print(0)
-    else:
-        chess=[[0 for i in range(size)] for i in range(size)]
+
+    chess=[[0 for i in range(size)] for i in range(size)]
 
 
-        bfs(currentX, currentY)
-        print(chess[wantX][wantY])
+    bfs(currentX, currentY)
+    print(chess[wantX][wantY])
+
 
 
 
