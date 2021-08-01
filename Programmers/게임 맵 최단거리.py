4e185606eef96f73d52a2fb93a76ae7fd98adb40
Choi33 <choi33ys@gmail.com>
2021-08-01 15:20:06 +0900

    수정

--- Programmers/게임 맵 최단거리.py
+++ Programmers/게임 맵 최단거리.py
@@ -14,7 +14,7 @@ def bfs(maps,visited,x,y,N,M):
             ny=positionY+dy[i]
             if ny<0 or nx<0 or nx>=N or ny>=M:
                 continue
-            if visited[nx][ny]==False:
+            if maps[nx][ny]!=0 and visited[nx][ny]==False:
                 visited[nx][ny]=True
                 que.append([nx,ny])
                 maps[nx][ny]=maps[positionX][positionY]+1
@@ -23,10 +23,7 @@ def solution(maps):
     N=len(maps)
     M=len(maps[0])
     visited=[[False for _ in range(M)] for _ in range(N)]
-    for a in range(N):
-        for b in range(M):
-            if maps[a][b]==0:
-                visited[a][b]=True
+
     bfs(maps,visited,0,0,N,M)
 
     if maps[-1][-1]!=1:
