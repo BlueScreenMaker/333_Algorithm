9c656d0ae7b9b704b737a5f69174fc70036bf919
Choi33 <choi333ys@gmail.com>
2021-10-12 14:01:15 +0900

    백준 2467

---
+++ BackJoon/2467.py
@@ -0,0 +1,29 @@
+import sys
+
+N=int(sys.stdin.readline())
+
+liquid=list(map(int,sys.stdin.readline().split()))
+
+left=0
+right=N-1
+
+check=1000000001*2
+answer1=0
+answer2=0
+
+while left<right:
+    temp=liquid[left]+liquid[right]
+    if check>abs(temp):
+        check=abs(temp)
+        answer1=liquid[left]
+        answer2=liquid[right]
+        if temp==0:
+            break
+    if temp<0:
+        left+=1
+    else :
+        right-=1
+
+
+print(answer1,answer2)
+
