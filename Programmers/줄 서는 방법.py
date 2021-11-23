b0b0cd7af6fedf6704ba38508932c190d0c9e5e2
Choi33 <choi333ys@gmail.com>
2021-11-23 18:23:44 +0900

    수정

--- Programmers/줄 서는 방법.py
+++ Programmers/줄 서는 방법.py
@@ -4,11 +4,11 @@ def solution(n, k):
     answer = []
     number_list=[i for i  in range(1,n+1)]
 
-    while n>0:
+    while n!=0:
         check=math.factorial(n-1)
-        index=k//check
+        index=(k-1)//check
         k = k % check
-        answer.append(number_list[index])
+        answer.append(number_list.pop(index))
         n-=1
     return answer
 
