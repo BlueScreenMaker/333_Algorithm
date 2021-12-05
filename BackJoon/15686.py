cf2301c2df38c70505d9d278916c60ec709e50e0
choi33 <choi33ys@gmail.com>
2021-12-05 18:56:56 +0900

    15686 미완성

--- BackJoon/15686.py
+++ BackJoon/15686.py
@@ -6,63 +6,61 @@ N,M=map(int,sys.stdin.readline().split())
 
 city=[]
 
-
 for i in range(N):
     city.append(list(map(int, sys.stdin.readline().split())))
 
 chicken=[]
-
 for i in range(N):
     for j in range(N):
         if city[i][j]==2:
             chicken.append([i,j])
 
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-def checking(x,y):
-    visited = [[False for _ in range(N)] for _ in range(N)]
-    que=deque()
-    que.append([x,y])
-    visited[x][y]=True
-    while que:
-        px,py=que.popleft()
-        for i in range(4):
-            nx=px+dx[i]
-            ny=py+dy[i]
-            if nx<0 or ny<0 or nx>=N or ny>=N:
-                continue
-            if city[nx][ny]==2:
-                result=abs(x-nx)+abs(y-ny)
-                return result
-            elif visited[nx][ny]==False:
-                visited[nx][ny]=True
-                que.append([nx,ny])
-    return 0
-
 number=len(chicken)
 answer=0
-min_answer = 101
+mid_answer = 101
+answer=0
 if number==M:
     for q in range(N):
         for p in range(N):
             if city[q][p]==1:
-                answer+=checking(q,p)
+                mid = 101
+                for t in range(number):
+                    nx=abs(q-chicken[t][0])
+                    ny=abs(p-chicken[t][1])
+                    total=abs(nx+ny)
+                    if mid>total:
+                        mid=total
+                answer+=mid
 
 else:
-    mid=0
+    count=0
     for candi in list(combinations(chicken, number-M)):
         for x, y in candi:
             city[x][y] = 0
         for a in range(N):
             for b in range(N):
                 if city[a][b]==1:
-                    mid+=checking(a,b)
-        if min_answer>mid:
-            min_answer=mid
+                    temp = 101
+                    for t in range(number):
+                        pX=chicken[t][0]
+                        pY=chicken[t][1]
+                        if(city[pX][pY]==2):
+                            nx = abs(a - pX)
+                            ny = abs(b - pY)
+                            total = abs(nx + ny)
+                            if temp > total:
+                                temp = total
+                    count+=temp
+        if mid_answer>count:
+            mid_answer=count
+        count=0
         for x, y in candi:
             city[x][y] = 2
 
-print(min_answer)
+
+if mid_answer!=101:
+    answer=mid_answer
+
 print(answer)
 
 
