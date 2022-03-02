1ca99bb9297b223b631346912559488669c6a3c4
Choi33 <choi333ys@gmail.com>
2022-03-02 21:30:17 +0900

    print문 지우기

--- Programmers/기능개발.py
+++ Programmers/기능개발.py
@@ -1,11 +1,10 @@
 def solution(progresses, speeds):
     answer = []
     count=0
-    
+
     while progresses:
         for i in range(len(progresses)):
             progresses[i]+=speeds[i]
-        print(progresses)
         if progresses[0]>=100:
             while progresses:
                 if progresses[0]>=100:
