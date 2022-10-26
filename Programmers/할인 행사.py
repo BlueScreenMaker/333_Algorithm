750a37bcadf2ea39983ab25e3fe7f366b5cf2409
YeSeul-0w0 <choi333ys@gmail.com>
2022-10-26 22:01:35 +0900

    프로그래머스 할인 행사

---
+++ Programmers/할인 행사.py
@@ -0,0 +1,20 @@
+def solution(want, number, discount):
+    answer = 0
+    total = sum(number)
+    length = len(discount)
+    shopping = dict()
+    for pair in zip(want, number):
+        shopping[pair[0]] = pair[1]
+    for i in range(length-total+1):
+        buy = dict()
+        for j in range(i,i+total):
+            if discount[j] in buy.keys():
+                buy[discount[j]] += 1
+            else:
+                buy[discount[j]] = 1
+        print(buy)
+        if buy == shopping:
+            answer += 1
+    return answer
+
+print(solution(["apple"],[10],["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"]))
