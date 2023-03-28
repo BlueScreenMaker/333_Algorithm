81f402c7da84f2ded505228a2ef76113227c116f
YeSeul-0w0 <choi333ys@gmail.com>
2023-03-28 09:29:49 +0900

    backjoon 16954

--- BackJoon/16954.py
+++ BackJoon/16954.py
@@ -1,61 +1,55 @@
 import sys
-import copy
 from collections import deque
 
 chess = []
 for a in range(8):
     chess.append(list(sys.stdin.readline().rstrip()))
 
+que = deque()
+que.append([7,0])
 visited = [[False for _ in range(8)] for _ in range(8)]
+dx = [0, -1, 1, 0, 0, -1, -1,1, 1]
+dy = [0, 0, 0, -1, 1, -1, 1, -1, 1]
 
 # 벽 위치 파악
-wall = []
-for x in range(8):
-    for y in range(8):
-        if chess[x][y] == "#":
-            wall.append([x, y])
+def move_wall():
+    temp_chess = [["." for _ in range(8)]for _ in  range(8)]
+    for i in range(7):
+        for j in range(8):
+            if chess[i][j] == "#":
+                temp_chess[i+1][j] = "#"
+    return temp_chess
 
-dx = [0, -1, 1, 0, 0, 1, 1,-1,-1]
-dy = [0, 0, 0, -1, 1, -1, 1, 1, -1]
-
-
-# 벽 내리기
-def down_wall():
-    temp_map = copy.deepcopy(wall)
-    for i in range(0, 7):
-        for j in range(0,7):
-            if temp_map[i][j] == "#":
-                temp_map[i+1][j] == "#"
-# 위 아래 좌 우
-def bfs(start_x, start_y):
-    que = deque()
-    que.append([start_x, start_y])
-    visited[start_x][start_y] = True
-    while que:
+# 턴 계산
+def bfs():
+    # 전 단계에서 가지고있던 경우의 수
+    length = len(que)
+    while length > 0:
         px, py = que.popleft()
-        if px == 0 and py == 7:
-            return True
-        else:
-            possible = []
-            for i in range(4):
-                nx = px + dx[i]
-                ny = py + dy[i]
-                if nx < 0 or ny < 0 or nx >= 8 or ny >= 8:
-                    continue
-                if not visited[nx][ny] and chess[nx][ny] == ".":
-                    if [nx, ny] in wall:
-                        possible.append(False)
-                    else:
-                        possible.append(True)
-                        visited[nx][ny] = True
-                        que.append([nx,ny])
-            down_wall()
-            if not all(possible):
-                return False
-    return False
+        length -= 1
 
-if bfs(7,0):
-    print(1)
-else:
-    print(0)
+        if chess[px][py] == "#":
+            continue
 
+        visited[px][py] = True
+        for i in range(9):
+            nx = px + dx[i]
+            ny = py + dy[i]
+            if 0 <= nx < 8 and 0 <= ny < 8:
+                if chess[nx][ny] == "." and not visited[nx][ny]:
+                    que.append([nx,ny])
+
+        # 제 자리를 선택할 경우
+        que.append([px,py])
+
+
+result = 1
+
+for i in range(8):
+    bfs()
+    if not que:
+        result = 0
+        break
+    chess = move_wall()
+
+print(result)
