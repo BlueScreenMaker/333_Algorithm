89b92d3d63bf031a7eeb8547f6cc9a3295dc497b
YeSeul-0w0 <choi333ys@gmail.com>
2024-11-22 10:00:01 +0900

    add comment

--- Additional/16934.py
+++ Additional/16934.py
@@ -16,6 +16,7 @@ class Trie(object):
         current_node = self.head
 
         for char in string:
+            # 새로운 문자가 기존 노드의 자식 노드에 없는 경우에만 새로운 노드를 생성
             if char not in current_node.children:
                 current_node.children[char] = Node(char)
             current_node = current_node.children[char]
@@ -27,6 +28,8 @@ class Trie(object):
         result = ""
         for char in string:
             result += char
+            # 문자열 탐색 중 현재 문자가 이미 트라이(Trie) 구조에 존재하는 경우,
+            # 해당 문자에 해당하는 자식 노드로 이동하도록 하는 것
             if char in current_node.children:
                 current_node = current_node.children[char]
             else:
